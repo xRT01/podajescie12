4d28a6d05e8a134eaf068e3e1632d81eb8ba2df1
xRT <oprobtom123@gmail.com>
2021-02-07 19:21:03 +0100

    zmiana na mnozenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+int policz (int a,int b){
+    return a*b;
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:: cout <<"W pierwszym branchu; 2*3=" << policz(2,3) <<std::endl;
     return 0;
 }
