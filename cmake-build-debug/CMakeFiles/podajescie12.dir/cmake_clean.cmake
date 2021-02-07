575372295c07977a4f97e7663cff948e86499920
xRT <oprobtom123@gmail.com>
2021-02-07 19:17:37 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/podajescie12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/podajescie12.dir/main.cpp.obj"
+  "podajescie12.exe"
+  "podajescie12.exe.manifest"
+  "podajescie12.lib"
+  "podajescie12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/podajescie12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
